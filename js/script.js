e14c7b413291fb45e7bbb7471fc43acd707ead12
Cesar Gomez <zootechdrum@gmail.com>
2019-04-11 22:21:15 -0700

    added responsive navbar to the header. Fixed overlay on mobile screens. Fixedd navbar overlay on about_us section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
 
-    console.log("Hello")
-})
+    $('.nav__mobileIcon').click( () => {
+        
+        const  nav = $('.nav__main');
+        const icon = $(".nav__mobile__menu");
+
+        nav.slideToggle(300);
+
+        if(icon.attr("name") === "menu" ){
+           icon.removeAttr("name");
+           icon.attr("name","close")
+        }else {
+            icon.removeAttr("name","close")
+            icon.attr("name","menu")
+        }
+        })
+    })
+
